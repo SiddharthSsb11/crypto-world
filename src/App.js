97160024cb3667b12538514e72e33afe785848b7
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-19 02:09:20 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from "@material-ui/core";
+import "./App.css";
+//import Banner from "./components/Banner/Banner";
+import Header from "./components/Header";
 
-function App() {
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+const App= () => {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <Header />
+      
     </div>
   );
 }
