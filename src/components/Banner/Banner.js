28b99c26cb25d54b65519af4f47e700e1ba98bee
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-19 20:07:54 +0530

    Banner and carousel for hompage added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,63 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles((theme) => ({
+  banner: {
+    backgroundImage: "url(./banner.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto World
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "gold",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
