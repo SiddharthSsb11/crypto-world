d7445bc2f32fe1767915798ad9e252a33399de97
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-20 21:26:47 +0530

    Coin Page and informational chart added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
 import "./App.css";
 //import Banner from "./components/Banner/Banner";
 import Header from "./components/Header";
+//import Banner from "./components/Banner/Banner";
+import HomePage from "./Pages/HomePage";
+import CoinPage from "./Pages/CoinPage";
 
 const useStyles = makeStyles(() => ({
   App: {
@@ -18,7 +23,10 @@ const App= () => {
   return (
     <div className={classes.App}>
       <Header />
-      
+      <Routes>
+        <Route path="/" element = {<HomePage />} />
+        <Route path="/coins/:id" element = {<CoinPage />} />
+      </Routes>  
     </div>
   );
 }
---
+++ src/components/CoinInfo.js
@@ -0,0 +1,130 @@
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { HistoricalChart } from "../config/api";
+import { Line } from "react-chartjs-2";
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import SelectButton from "./SelectButton";
+import { chartDays } from "../config/data";
+import CryptoContext from "../store/crypto-context";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+      width: "75%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 25,
+      padding: 40,
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+        marginTop: 0,
+        padding: 20,
+        paddingTop: 0,
+      },
+    },
+}));
+
+const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+});
+
+const CoinInfo = ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = useContext(CryptoContext);
+  const [flag,setflag] = useState(false);
+
+  const classes = useStyles();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    console.log('historic data chart', data );
+    setflag(true);
+    setHistoricData(data.prices);
+  };
+
+  //console.log(coin);
+
+  useEffect(() => {
+    fetchHistoricData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [days]);
+
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {!historicData | flag===false ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#EEBC1D",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+            <div
+              style={{
+                display: "flex",
+                marginTop: 20,
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              {chartDays.map((day) => (
+                <SelectButton
+                  key={day.value}
+                  onClick={() => {setDays(day.value);
+                    setflag(false);
+                  }}
+                  selected={day.value === days}
+                >
+                  {day.label}
+                </SelectButton>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default CoinInfo;
