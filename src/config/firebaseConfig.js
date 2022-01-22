79c550fda6787dd1e4fc87c0b1c1e33c9fe2ae8e
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-22 21:20:58 +0530

    Firebase as a backend service added

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0CM4J3doIB-UYZ2AsAXLaKsa2MWpEvQ4",
+  authDomain: "crypto-world-86cfc.firebaseapp.com",
+  projectId: "crypto-world-86cfc",
+  storageBucket: "crypto-world-86cfc.appspot.com",
+  messagingSenderId: "1053445340910",
+  appId: "1:1053445340910:web:63bda57c054742c9933dff"
+};
+
+export default firebaseConfig;
