cd2cdaf9a6bc319718273ec8e3e47ac0e3f4bc21
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-22 22:28:57 +0530

    logged in user sidebar added

---
+++ src/components/Authentication/UserSidebar.js
@@ -0,0 +1,157 @@
+import React, {useContext, useState} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import { Avatar, Button } from "@material-ui/core";
+import CryptoContext from "../../store/crypto-context";
+import { signOut } from "firebase/auth";
+import { auth, /* db */ } from "../../firebase";
+/* import { numberWithCommas } from "../CoinsTable";
+import { AiFillDelete } from "react-icons/ai";
+import { doc, setDoc } from "firebase/firestore"; */
+
+const useStyles = makeStyles({
+  container: {
+    width: 350,
+    padding: 25,
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    fontFamily: "monospace",
+  },
+  profile: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+    height: "92%",
+  },
+  logout: {
+    height: "8%",
+    width: "100%",
+    backgroundColor: "#EEBC1D",
+    marginTop: 20,
+  },
+  picture: {
+    width: 200,
+    height: 200,
+    cursor: "pointer",
+    backgroundColor: "#EEBC1D",
+    objectFit: "contain",
+  },
+  watchlist: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "grey",
+    borderRadius: 10,
+    padding: 15,
+    paddingTop: 10,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 12,
+    overflowY: "scroll",
+  },
+  coin: {
+    padding: 10,
+    borderRadius: 5,
+    color: "black",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#EEBC1D",
+    boxShadow: "0 0 3px black",
+  },
+});
+
+export default function UserSidebar() {
+  const classes = useStyles();
+  const [state, setState] = useState({right: false});
+  const { user, setAlert } = useContext(CryptoContext);
+
+  //console.log(watchlist, coins);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const logOut = () => {
+    signOut(auth);
+    setAlert({
+      open: true,
+      type: "success",
+      message: "Logout Successfull !",
+    });
+
+    toggleDrawer();
+  };
+
+
+  return (
+    <div>
+      {["right"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Avatar
+            onClick={toggleDrawer(anchor, true)}
+            style={{
+              height: 38,
+              width: 38,
+              marginLeft: 15,
+              cursor: "pointer",
+              backgroundColor: "#EEBC1D",
+            }}
+            src={user.photoURL}
+            alt={user.displayName || user.email}
+          />
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            <div className={classes.container}>
+              <div className={classes.profile}>
+                <Avatar
+                  className={classes.picture}
+                  src={user.photoURL}
+                  alt={user.displayName || user.email}
+                />
+                <span
+                  style={{
+                    width: "100%",
+                    fontSize: 25,
+                    textAlign: "center",
+                    fontWeight: "bolder",
+                    wordWrap: "break-word",
+                  }}
+                >
+                  {user.displayName || user.email}
+                </span>
+                <div className={classes.watchlist}>
+                  <span style={{ fontSize: 20, textShadow: "0 0 5px black", fontweight: "bold" , borderBottom: "1px solid"}}>
+                    Watchlist
+                  </span>
+                  
+                </div>
+              </div>
+              <Button
+                variant="contained"
+                className={classes.logout}
+                onClick={logOut}
+              >
+                Log Out
+              </Button>
+            </div>
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
